3539209f3d845e7cdb93ac2ad35935d9a0f95f82
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-31 19:46:50 +0530

    Update practice.js

--- for in loop/practice.js
+++ for in loop/practice.js
@@ -10,3 +10,26 @@ let list = ["one", "two", "three", "four"];
 for(let index in list){
     console.log(index,":",list[index]);
 }
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+    console.log(`${property}: ${object[property]}`);
+}
+const testScores = {
+       Math: 90,
+       Science: 85,
+       History: 78,
+       English: 92,
+       Geography: 88,
+};
+function calculateaveragescore(score){
+    let totalscore=0;
+    let num=0;
+    for(let subject in score){
+        totalscore=totalscore+score[subject];
+        num=num+1;
+        
+    }
+    return totalscore / num;
+}
+console.log(calculateaveragescore(testScores));
