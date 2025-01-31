e0afe030a7f9dce9fc44f41ee62905439a2855a0
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-31 19:14:12 +0530

    used for in loop
    Used to iterate over the enumerable properties of an object

---
+++ for in loop/practice.js
@@ -0,0 +1,12 @@
+let person={
+    name:"Adit",
+    age:"19",
+    profession:"student"
+};
+for(let key in person){
+    console.log(key,":",person[key]);
+}
+let list = ["one", "two", "three", "four"];
+for(let index in list){
+    console.log(index,":",list[index]);
+}
