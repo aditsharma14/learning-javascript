2f98104797c82f82dabaa5169914af611c95529f
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-02-01 17:46:27 +0530

    for of loop
    The "for...of" loop in JavaScript is a modern iteration statement introduced in ECMAScript 6 (ES6) that provides a concise and easy way to loop over elements in iterable objects like arrays, strings, maps, sets, and more. It allows you to iterate directly over the values of the elements, rather than dealing with their indices or keys, which makes the code more readable and less error-prone.

---
+++ for of loop/practice.js
@@ -0,0 +1,13 @@
+let peoples = ["adit", "ram", "aditya", "ishaan"];
+for(let people of peoples){
+    console.log(people);
+}
+const text="hello";
+for(const char of text){
+    console.log(char);
+}
+const array1 = ["a", "b", "c"];
+
+for (const element of array1) {
+  console.log(element);
+}
