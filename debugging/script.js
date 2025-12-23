15c8726030b0e35ef1456bd8158066a4cc1fe94a
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-12-23 21:05:32 +0530

    Modified some code mistakes in the javascript code

--- debugging/script.js
+++ debugging/script.js
@@ -2,8 +2,8 @@ const { parse } = require("path");
 
 function performOperation() {
     let num1=parseInt(document.getElementById("input1").value,10);
-    let num2=parseInt(document.getElementById("input").value,10);
-    if(!NaN(num1)&&!NaN(num2)){
+    let num2=parseInt(document.getElementById("input2").value,10);
+    if(!isNaN(num1)&&!isNaN(num2)){
         let result=multiply(num1,num2);
             displayResult(result);
 
